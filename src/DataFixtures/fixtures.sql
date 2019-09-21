2d34de0ae0954c910b2ec66ca80a80dc1131a258
Denis Limarev <dlimarev@wakeapp.ru>
2019-09-21 22:36:54 +0300

    fixes in pdo and fixtures

---
+++ src/DataFixtures/fixtures.sql
@@ -0,0 +1,6 @@
+# 😀
+INSERT INTO smile (code, sample) values (128512, 'happy.ogg');
+# 😂
+INSERT INTO smile (code, sample) values (128514, 'laugh.ogg');
+# 😑
+INSERT INTO smile (code, sample) values (128529, 'sss.ogg');
