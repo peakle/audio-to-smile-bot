17b7a9a43b071abe9b6cff1bbc5813526ab55985
Denis Limarev <dlimarev@wakeapp.ru>
2019-09-23 00:26:01 +0300

    delete some bundles

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
