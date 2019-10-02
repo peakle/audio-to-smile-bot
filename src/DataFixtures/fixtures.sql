f1371e7a451f9d29af64cc1ebfdc93d00f03e4a7
Denis Limarev <dlimarev@wakeapp.ru>
2019-10-03 01:23:19 +0300

    add audios

--- src/DataFixtures/fixtures.sql
+++ src/DataFixtures/fixtures.sql
@@ -1,6 +1,18 @@
 # 😀
-INSERT INTO smile (code, sample) values (128512, 'happy.ogg');
+INSERT INTO smile (code, sample) values (128512, 'smile.ogg');
 # 😂
 INSERT INTO smile (code, sample) values (128514, 'laugh.ogg');
-# 😑
-INSERT INTO smile (code, sample) values (128529, 'sss.ogg');
+# 😑😬
+INSERT INTO smile (code, sample) values (128529, 'ss.ogg');
+INSERT INTO smile (code, sample) values (128556, 'ss.ogg');
+# 🐊
+INSERT INTO smile (code, sample) values (128010, 'crocodile.ogg');
+# 🍑
+INSERT INTO smile (code, sample) values (127825, 'peach.ogg');
+# 😤
+INSERT INTO smile (code, sample) values (128548, 'proud.ogg');
+# 💩
+INSERT INTO smile (code, sample) values (128169, 'shit.ogg');
+# 🧐🤔
+INSERT INTO smile (code, sample) values (129488, 'think.ogg');
+INSERT INTO smile (code, sample) values (129300, 'think.ogg');
